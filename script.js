f04615270203ff13f7db3eb7d4bc1688bfb110fe
Jorge Basilico <jabasilico@gmail.com>
2023-07-07 12:21:43 -0300

    Actualizar archivos

--- script.js
+++ script.js
@@ -1,5 +1,31 @@
-function searchContacts() {
-  let input = document.getElementById("searchInput");
+function searchContactsByCode() {
+  let input = document.getElementById("searchInputCode");
+  let filter = input.value.toUpperCase();
+  let table = document.getElementById("contactsTable");
+  let tr = table.getElementsByTagName("tr");
+
+  for (let i = 0; i < tr.length; i++) {
+    let td = tr[i].getElementsByTagName("td")[0];
+    if (td) {
+      let textValue = td.textContent || td.innerText;
+      if (textValue.toUpperCase().indexOf(filter) > -1) {
+        tr[i].style.display = "";
+      } else {
+        tr[i].style.display = "none";
+      }
+    }
+  }
+}
+
+function clearSearch() {
+  document.getElementById("searchInputName").value = "";
+  document.getElementById("searchInputCode").value = "";
+  searchContactsByName();
+  searchContactsByCode();
+}
+
+function searchContactsByName() {
+  let input = document.getElementById("searchInputName");
   let filter = input.value.toUpperCase();
   let table = document.getElementById("contactsTable");
   let tr = table.getElementsByTagName("tr");
